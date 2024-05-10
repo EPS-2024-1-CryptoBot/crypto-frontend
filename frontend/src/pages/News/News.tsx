d31c155efe1f9ed17f07314125540a93afccdbdf
Ailton Aires <ailtonaires5@gmail.com>
2024-05-10 18:12:58 -0300

    Ajustando API de notícias

--- frontend/src/pages/News/News.tsx
+++ frontend/src/pages/News/News.tsx
@@ -5,10 +5,12 @@ const News = () => {
 
 
     const formatDate = (date: Date) => {
-        const year = date.getFullYear();
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const day = date.getDate().toString().padStart(2, '0');
-        return `${year}-${day}-${month}`;
+        console.log(date);
+        // const year = date.getFullYear();
+        // const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        // const day = date.getDate().toString().padStart(2, '0');
+        // return `${year}-${day}-${month}`;
+        return "2024-05-05";
     }
     useEffect(() => {
         const fetchNews = async () => {
