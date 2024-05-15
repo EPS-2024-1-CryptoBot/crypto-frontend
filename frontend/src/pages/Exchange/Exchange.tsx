be2b5f2bdbbb1a0e1ee27c837b54867c22e7cb48
Kevin Luis <k.luis360@gmail.com>
2024-05-15 20:32:38 -0300

    feat: Add get balance request

--- frontend/src/pages/Exchange/Exchange.tsx
+++ frontend/src/pages/Exchange/Exchange.tsx
@@ -1,15 +1,25 @@
-import { api } from "../../config/api";
+import { api } from '../../config/api';
 
 const Exchange = () => {
   const handleMine = async () => {
-    const response = await api.post("/wallet/mine");
+    const response = await api.post('/wallet/mine');
 
     console.log(response.data);
   };
+
+  const handleBalance = async () => {
+    const response = await api.get('/wallet/balance');
+
+    console.log(response.data);
+  };
+
   return (
     <div className="w-full h-full text-center m-5">
       <button className="bg-primary text-white rounded p-4" onClick={handleMine}>
         Mine
+      </button>
+      <button className="bg-secondary text-white rounded p-4 ml-5" onClick={handleBalance}>
+        Get Balance
       </button>
     </div>
   );
