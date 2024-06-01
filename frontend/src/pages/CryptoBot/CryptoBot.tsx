f78ea29045bfac2d9207ef558b947c9d04ffced8
jvsdurso <jvsdurso@gmail.com>
2024-05-31 22:21:07 -0300

    Adicionando início do bot

--- frontend/src/pages/CryptoBot/CryptoBot.tsx
+++ frontend/src/pages/CryptoBot/CryptoBot.tsx
@@ -1,5 +1,9 @@
-import { AuthContext } from '../../contexts/authContext';
 import { useContext } from 'react';
+import { AuthContext } from '../../contexts/authContext';
+import Logging from "./components/Logging";
+import Strategies from "./components/Strategies";
+import Trade from "./components/Trade";
+import Watchlist from "./components/Watchlist";
 import { addApiKeyBinanceToUser, decryptApiKeyBinance } from './routes';
 
 const CryptoBot = () => {
@@ -25,6 +29,7 @@ const CryptoBot = () => {
       // if(res.api_token_binance !== null) {
       //   alert('Chave criada com sucesso!');
       // }
+      
     } catch (error) {
       alert(JSON.stringify(error));
     }
@@ -32,10 +37,33 @@ const CryptoBot = () => {
 
   return (
     <div>
-      <h1>CryptoBot</h1>
-      <button onClick={salvarChave}>GERAR CHAVE</button>
-      <div></div>
-      <button onClick={pegarChave}>PEGAR CHAVE</button>
+      {/* <button 
+        onClick={salvarChave} 
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 mr-4"
+      >
+        GERAR CHAVE
+      </button>
+      <button 
+        onClick={pegarChave} 
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >
+        PEGAR CHAVE
+      </button> */}
+      <div className="w-full h-[90vh] bg-gray-900 text-white flex flex-wrap p-4 mt-4 mx-auto ">
+        <div className="w-[40%] h-[50%] bg-red-500 flex items-center justify-center">
+          <Trade />
+        </div>
+        <div className="w-[60%] h-[50%] bg-blue-500 flex items-center justify-center">
+          <Strategies />
+        </div>
+        <div className="w-[40%] h-[50%] bg-green-500 flex items-center justify-center">
+          <Logging />
+        </div>
+        <div className="w-[60%] h-[50%] bg-yellow-500 flex items-center justify-center">
+          <Watchlist />
+        </div>
+      </div>
+
     </div>
   );
 };
