a8734af2bb9d11f18534f877ead1978044afb0f9
Kevin Luis <k.luis360@gmail.com>
2024-06-23 19:05:36 -0300

    feat: Add chart component for displaying stock prices

--- frontend/src/pages/ConsultorAcoes/DetalhesAcao.tsx
+++ frontend/src/pages/ConsultorAcoes/DetalhesAcao.tsx
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import { api } from '../../config/api';
 import { formatBrlValue } from '../../utils/utils';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+// Registro dos componentes necessários do Chart.js
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const DetalhesAcao = () => {
   const [searchTicker, setSearchTicker] = useState('');
@@ -22,6 +27,19 @@ const DetalhesAcao = () => {
     }
   };
 
+  const chartData = {
+    labels: companyData ? companyData.allPrices.map((item:any) => item?.priceDate) : [],
+    datasets: [
+      {
+        label: 'Preço',
+        data: companyData ? companyData.allPrices.map((item:any) => item?.value) : [],
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        fill: true,
+      },
+    ],
+  };
+
   return (
     <div className="container max-w-[60%] p-4">
       <h1 className="text-2xl font-bold mb-4">Busca por Ticker</h1>
@@ -37,41 +55,51 @@ const DetalhesAcao = () => {
           Buscar
         </button>
       </div>
-      {!loadingTickers && companyData && (
-        <div className="bg-white shadow rounded p-4">
-          <h2 className="text-xl font-bold mb-2">{companyData?.companyName}</h2>
-          <p>
-            <strong>Ticker:</strong> {companyData?.ticker}
-          </p>
-          <p>
-            <strong>Preço Atual:</strong> R$ {companyData?.currentPrice}
-          </p>
-          <p>
-            <strong>CompanyId:</strong> {companyData?.companyId}
-          </p>
-          <p>
-            <strong>Free Float:</strong> {companyData?.freeFloat}%
-          </p>
-          <p>
-            <strong>Tag Along:</strong> {companyData?.tagAlong}%
-          </p>
-          <p>
-            <strong>Liquidez Diária Média:</strong> R$ {formatBrlValue(companyData?.avgDailyLiquidity)}
-          </p>
-          <p>
-            <strong>Categoria:</strong> {companyData?.categorie}
-          </p>
-          <p>
-            <strong>Variação 1 Dia:</strong> {companyData?.variationOneDay}%
-          </p>
-          <p>
-            <strong>Variação 1 Mês:</strong> {companyData?.variationOneMonth}%
-          </p>
-          <p>
-            <strong>Variação 12 Meses:</strong> {companyData?.variationTwelveMonths}%
-          </p>
-        </div>
-      )}
+      <div className="w-full flex flex-col gap-2">
+        {!loadingTickers && companyData && (
+          <Fragment>
+            <div className="bg-white shadow rounded p-4">
+              <h2 className="text-xl font-bold mb-2">{companyData?.companyName}</h2>
+              <p>
+                <strong>Ticker:</strong> {companyData?.ticker}
+              </p>
+              <p>
+                <strong>Preço Atual:</strong> R$ {companyData?.currentPrice}
+              </p>
+              <p>
+                <strong>CompanyId:</strong> {companyData?.companyId}
+              </p>
+              <p>
+                <strong>Free Float:</strong> {companyData?.freeFloat}%
+              </p>
+              <p>
+                <strong>Tag Along:</strong> {companyData?.tagAlong}%
+              </p>
+              <p>
+                <strong>Liquidez Diária Média:</strong> R${' '}
+                {formatBrlValue(companyData?.avgDailyLiquidity)}
+              </p>
+              <p>
+                <strong>Categoria:</strong> {companyData?.categorie}
+              </p>
+              <p>
+                <strong>Variação 1 Dia:</strong> {companyData?.variationOneDay}%
+              </p>
+              <p>
+                <strong>Variação 1 Mês:</strong> {companyData?.variationOneMonth}%
+              </p>
+              <p>
+                <strong>Variação 12 Meses:</strong> {companyData?.variationTwelveMonths}%
+              </p>
+            </div>
+            <div className="bg-white shadow rounded p-4">
+              <div className="mt-4">
+                <Line data={chartData} options={{ responsive: true, maintainAspectRatio: false }} />
+              </div>
+            </div>
+          </Fragment>
+        )}
+      </div>
     </div>
   );
 };
