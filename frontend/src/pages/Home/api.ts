b140922d195e83873391fb246bd169b93c652c71
Ailton Aires <ailtonaires5@gmail.com>
2024-06-26 10:30:40 -0300

    Corrigindo api do bot

--- frontend/src/pages/Home/api.ts
+++ frontend/src/pages/Home/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://pitchit-api.onrender.com/api/', // URL base da API
+  baseURL: 'https://pitchit-api.onrender.com/', // URL base da API
   headers: {
     'Content-Type': 'application/json'
   }
